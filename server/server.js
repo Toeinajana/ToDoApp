af6ec883d45f3a040fc4e4ce25925e3a0da30c0f
Nantawan G <toeinajana@hotmail.com>
2019-04-21 13:42:16 +1000

    fix jquery link

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+// var cors = require('cors')
+const app = express()
+const port = 3000
+
+// app.use(cors())
+
+
+app.listen(port,() => console.log('successfully connected'))
