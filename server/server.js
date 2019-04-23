8565df4ec51b57010e28069423f6f9ce5853afe9
Nantawan G <toeinajana@hotmail.com>
2019-04-23 13:05:32 +1000

    "modified frontend and connect server"

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 const express = require('express')
-// var cors = require('cors')
-const app = express()
+var cors = require('cors')
+const app = express();
 const port = 3000
 
-// app.use(cors())
+var mongoose = require("mongoose");
+
+// mongoose connection
+mongoose.connect("mongodb://localhost/todoapp");
+
+app.use(cors())
+
+//////// express route ////////
+app.get("/",function(reg, res){
+
+    res.send("<h1>Hello tj</h1>");
 
 
-app.listen(port,() => console.log('successfully connected'))
+});
+
+///server listener on port
+
+app.listen(3000, function() {
+
+console.log("server started on port 3000");
+
+
+});
+
+// app.listen(port,() => console.log('successfully connected'))
